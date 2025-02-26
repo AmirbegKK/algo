75afa209cb7ff4d5f251ffa4bd4679e14b2a6954
Amirbeg <avastfsoc541@gmail.com>
2025-02-27 02:07:55 +0300

    Linked List 2.1

---
+++ LinkedList.py
@@ -0,0 +1,81 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Llist:
+    def __init__(self, nodes):
+        if nodes is not None:
+            node = Node(nodes.pop(0))
+            self.head = node
+            while nodes:
+                node.next = Node(nodes.pop(0))
+                node = node.next
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append('None')
+        return ' -> '.join(nodes)
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def add_first(self, node):
+        node.next = self.head
+        self.head = node
+
+    def add_last(self, node):
+        for el in self:
+            pass
+        el.next = node
+
+    def remove(self, node):
+        while self.head.data == node.data:
+            self.head = self.head.next
+
+        prev = self.head
+        cur = self.head.next
+        while cur is not None:
+            if cur.data == node.data:
+                prev.next = cur.next
+                cur = cur.next
+                return
+            else:
+                prev = cur
+                cur = cur.next
+
+def remove_duplicates_naive(linked_list):
+    node_values = set()
+    for elem in linked_list:
+        if elem.data in node_values:
+            linked_list.remove(elem)
+        else:
+            node_values.add(elem.data)
+
+def remove_duplicates_fast(linked_list):
+    current_node = linked_list.head
+    while current_node is not None:
+        runner_node = current_node
+        while runner_node.next is not None:
+            if current_node.data == runner_node.next.data:
+                runner_node.next = runner_node.next.next
+            else:
+                runner_node = runner_node.next
+
+        current_node = current_node.next
+
+
+if __name__ == '__main__':
+    linked_list_ = Llist(['1', '2', '3', '4', '5', '3'])
+    print(linked_list_)
+
+
+    remove_duplicates_fast(linked_list_)
